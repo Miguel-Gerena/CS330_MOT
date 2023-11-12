beb77a18be3e31c79270cba90cf04ba6ed9d69ee
akayla hackson <akayla.hackson11@gmail.com>
2023-11-12 15:51:49 -0800

    util file used in maml

--- util.py
+++ util.py
@@ -11,10 +11,21 @@ def score(logits, labels):
         labels (torch.Tensor): classification labels from 0 to num_classes - 1
             shape (examples,)
     """
+    try:
+        assert logits.dim() == 2
+        assert labels.dim() == 1
+        assert logits.shape[0] == labels.shape[0]
+    except AssertionError:
+        raise ValueError(
+            f"Input shapes are invalid: logits shape {logits.shape}, labels shape {labels.shape}"
+        )
 
-    assert logits.dim() == 2
-    assert labels.dim() == 1
-    assert logits.shape[0] == labels.shape[0]
     y = torch.argmax(logits, dim=-1) == labels
     y = y.type(torch.float)
     return torch.mean(y).item()
+
+def calculate_accuracy(logits, labels):
+    predicted_classes = torch.argmax(logits.squeeze(0), dim=-1)
+    true_classes = labels.squeeze(0)
+    accuracy = (predicted_classes == true_classes).float().mean().item()
+    return accuracy
