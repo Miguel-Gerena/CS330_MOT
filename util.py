0b85ec6f12421548209ba977d64e11bd8f917327
akayla hackson <akayla.hackson11@gmail.com>
2023-11-13 21:39:06 -0800

    Iou calculation for bbox

--- util.py
+++ util.py
@@ -25,7 +25,44 @@ def score(logits, labels):
     return torch.mean(y).item()
 
 def calculate_accuracy(logits, labels):
-    predicted_classes = torch.argmax(logits.squeeze(0), dim=-1)
-    true_classes = labels.squeeze(0)
-    accuracy = (predicted_classes == true_classes).float().mean().item()
+
+    # Flatten both logits and labels to (batch_size * num_classes,)
+    logits = logits.reshape(-1)
+    labels = labels.reshape(-1)
+
+    predicted_classes = torch.argmax(logits, dim=-1)
+    accuracy = (predicted_classes == labels).float().mean().item()
     return accuracy
+
+
+def calculate_iou(predicted_bboxes, true_bboxes):
+    """
+    Calculate the Intersection over Union (IoU) for bounding boxes.
+
+    Args:
+    - predicted_bboxes (Tensor): Predicted bounding boxes, shape [batch_size, num_players, 4]
+    - true_bboxes (Tensor): True bounding boxes, shape [batch_size, num_players, 4]
+
+    Returns:
+    - float: Mean IoU over all bounding boxes
+    """
+    # Coordinates of the intersection boxes
+    inter_left = torch.max(predicted_bboxes[..., 0], true_bboxes[..., 0])
+    inter_top = torch.max(predicted_bboxes[..., 1], true_bboxes[..., 1])
+    inter_right = torch.min(predicted_bboxes[..., 0] + predicted_bboxes[..., 2], true_bboxes[..., 0] + true_bboxes[..., 2])
+    inter_bottom = torch.min(predicted_bboxes[..., 1] + predicted_bboxes[..., 3], true_bboxes[..., 1] + true_bboxes[..., 3])
+
+    # Intersection area
+    inter_area = torch.clamp(inter_right - inter_left, min=0) * torch.clamp(inter_bottom - inter_top, min=0)
+
+    # Union area
+    pred_area = predicted_bboxes[..., 2] * predicted_bboxes[..., 3]
+    true_area = true_bboxes[..., 2] * true_bboxes[..., 3]
+    union_area = pred_area + true_area - inter_area
+
+    iou = inter_area / torch.clamp(union_area, min=1e-6)
+
+    # Handle cases where IoU is NaN
+    iou[torch.isnan(iou)] = 0
+
+    return iou.mean().item()
